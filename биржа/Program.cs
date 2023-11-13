db08b4c7f0e7fe326ddb141a40f1e9afe4bd9099
Eketrin <Katebaiko@yandex.ru>
2023-11-13 17:09:22 +0300

    готово, но нужно доработать

--- биржа/Program.cs
+++ биржа/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿
+using System;
+
+using System.Xml;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +9,59 @@ using System.Threading.Tasks;
 
 namespace биржа
 {
+    public interface IJsonAnalyticsLibrary
+    {
+        string AnalyzeJsonData(string jsonData);
+    }
+    // Класс библиотеки аналитики, работающей с JSON
+    public class JsonAnalyticsLibrary : IJsonAnalyticsLibrary
+    {
+        public string AnalyzeJsonData(string jsonData)
+        {
+            // Реализация анализа данных в формате JSON
+            // ...
+            return "Analysis result in JSON format";
+        }
+    }
+    // Интерфейс адаптера для поддержки работы с XML
+    public interface IXmlAdapter
+    {
+        string AnalyzeXmlData(string xmlData);
+    }
+    // Адаптер, преобразующий вызовы методов библиотеки аналитики из XML в JSON
+    public class XmlAdapter : IXmlAdapter
+    {
+        private readonly IJsonAnalyticsLibrary jsonAnalyticsLibrary;
+        public XmlAdapter(IJsonAnalyticsLibrary jsonAnalyticsLibrary)
+        {
+            this.jsonAnalyticsLibrary = jsonAnalyticsLibrary;
+        }
+        public string AnalyzeXmlData(string xmlData)
+        {
+            // Преобразование XML в JSON (без использования Newtonsoft.Json)
+            string jsonData = ConvertXmlToJson(xmlData);
+            // Вызов метода анализа данных в формате JSON
+            return jsonAnalyticsLibrary.AnalyzeJsonData(jsonData);
+        }
+        private string ConvertXmlToJson(string xmlData)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlData);
+            string jsonData = "{ \"example\": \"data\" }";
+            return jsonData;
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // Пример использования адаптера
+            IJsonAnalyticsLibrary jsonAnalyticsLibrary = new JsonAnalyticsLibrary();
+            IXmlAdapter xmlAdapter = new XmlAdapter(jsonAnalyticsLibrary);
+            string xmlData = "<xml>...</xml>";
+            string analysisResult = xmlAdapter.AnalyzeXmlData(xmlData);
+            Console.WriteLine("Analysis Result: " + analysisResult);
+            Console.ReadLine();
         }
     }
 }
